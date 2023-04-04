22b0faf3a8b8378626ce6eff2a639627e3c43a21
lado8 <talakvadze99@gmail.com>
2023-04-05 00:34:38 +0400

    day2.2 code commit

---
+++ Day2.2/lesson2.2.py
@@ -0,0 +1,19 @@
+name = "lado"
+print(len(name))
+my_sentence = """ i am from georgia 
+i live in tbilisi """
+print(my_sentence)
+kniws_programming=True
+if 8>4:
+    print("hello")
+full_name = "lado talakvadze"
+print(len(full_name))
+print(full_name[8])
+print(full_name[3:7:2])
+print("k" in full_name)
+print("t" not in full_name)
+name = "talakvadze"
+print(name[-2:-5:-2])
+print(name.upper())
+print(name.replace(" ","*"))
+

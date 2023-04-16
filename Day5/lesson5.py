94fa02f476dbe1b92fb86abd2306a0e5cd549e8c
lado8 <talakvadze99@gmail.com>
2023-04-16 21:34:07 +0400

    day5 code commit

---
+++ Day5/lesson5.py
@@ -0,0 +1,43 @@
+# x - საიტერაციო ცვლადი
+for x in range(1,8):
+    print("lado")
+    x += 1
+# x += 1 თუ ამ ცვლადს დავპრინტავთ მივიღებთ "lado" - ს უსასრულოდ
+
+# ჩავწერ სხვაობით
+for x in range(5-9)
+print("lado")
+x += 1
+
+# range(5,10,2) - 5 დან 10 ჩათვლით ყოველი მესამე
+for x in range(5,10,2):
+    print("lado")
+    x += 1
+
+# სანამ ნაკლებია 8 ზე იპრინტება "lado"
+x=0
+while x < 8:
+    print(lado)
+    x += 1
+# N-1
+full_name = "lado talakvadze"
+x = 0
+while x < len(full_name):
+    print(full_name[x])
+    x += 1  
+# შემოვიტანოთ a და b , ტერმინალში დაპრინტვის შედეგად უნდა მივიღოთ AE, BF, CG, DH
+a="ABCD"
+b="EFGH"
+x = 0
+while x < len(a):
+    print(a[x]+b[x])
+    x += 1
+# and - კავშირში ერთ-ერთი თუ არასწორია უკვე მცდარია,მხოლოდ ორივეს ჭეშმარიტების შემთხვევაშია True
+if 11 > 8 and 3 < 5:
+    print("cool")
+
+#2
+if (1==1) and (3+8>5):
+    print("yes")
+else:
+    print("no")
